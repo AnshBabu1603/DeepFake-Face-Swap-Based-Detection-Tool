0495e3b0c2d8237bb988e2ad7205574dbdcab29b
AnshBabu1603 <raj.anshbabu.ab@gmail.com>
2026-05-14 09:57:50 +0530

    Update API_BASE_URL to new ngrok address

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
 
 // API Configuration
-export const API_BASE_URL = ' https://9031-2409-4055-51f-d791-119b-e910-fc64-1264.ngrok-free.app';
+export const API_BASE_URL = ' https://2738-2409-4055-19-ea8d-d41d-8dfd-abf9-69d7.ngrok-free.app';
